02ae2abcc0b26ad6c33be853f552d696c9da69ed
sircuddles <governmentslave@hotmail.com>
2016-02-27 21:20:53 -0500

    Video 3 - Add tile prefabs for level generation

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+    private Vector3 direction;
+
+    void Start() {
+        direction = Vector3.zero;
+    }
+
+    void Update() {
+        // Handle mouse input
+        if (Input.GetMouseButtonDown(0)) {
+            if (direction == Vector3.forward) {
+                direction = Vector3.left;
+            }
+            else {
+                direction = Vector3.forward;
+            }
+        }
+
+        // Move the player
+        float amountToMove = speed * Time.deltaTime;
+        transform.Translate(direction * amountToMove);
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+    [SerializeField]
+    private GameObject leftTilePrefab;
+    [SerializeField]
+    private GameObject topTilePrefab;
+    public GameObject currentTile;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < 10; i++) {
+            SpawnTile();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SpawnTile() {
+        currentTile = (GameObject)Instantiate(leftTilePrefab, currentTile.transform.GetChild(0).transform.GetChild(0).transform.position, Quaternion.identity);
+        
+    }
+}
